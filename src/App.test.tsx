50d993b3fb5dc610967c40277211525a5c80e402
Emily Pummeroy <emilypummeroy@gmail.com>
2026-03-27 22:20:24 +1100

    -: Add linting and formatting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,45 +1,43 @@
-import { describe, it, expect, beforeEach } from 'vitest';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+
 import App from './App.tsx';
+
+const FEW = 3;
 
 describe('the dom test environment', () => {
   it('should have a defined document', () => {
-    expect(typeof document).not.toBe('undefined');
+    expectTypeOf(document).not.toBeUndefined();
   });
 });
 
 describe(App, () => {
   beforeEach(() => render(<App />));
 
-  it("should have the wartide heading", async () => {
+  it('should have the wartide heading', async () => {
     const heading = await screen.findByRole('heading', { level: 1 });
-
     expect(heading).toHaveTextContent('Wartide');
-    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Wartide');
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'Wartide',
+    );
   });
 
   describe('Phase', () => {
-    it("should start in the main phase", async () => {
+    it('should start in the main phase', async () => {
       expect(await screen.findByLabelText('Phase')).toHaveTextContent('Main');
     });
 
-    it("should advance to the end phase when the button is clicked", async () => {
+    it('should advance to the end phase when the button is clicked', async () => {
       fireEvent.click(await screen.findByText('Next phase'));
-
       expect(screen.getByLabelText('Phase')).toHaveTextContent('End');
     });
 
-    it("should cycle between main and end phase when the button is clicked", async () => {
-      for await (let _ of Array(3)) {
+    it('should cycle between main and end phase when the button is clicked', async () => {
+      for (let x = 0; x < FEW; x += 1) {
         fireEvent.click(await screen.findByText('Next phase'));
-
         expect(screen.getByLabelText('Phase')).toHaveTextContent('End');
-
         fireEvent.click(await screen.findByText('Next phase'));
-
         expect(screen.getByLabelText('Phase')).toHaveTextContent('Main');
       }
     });
   });
 });
-  
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
+import './App.css';
 import { useState } from 'react';
-import './App.css'
 
 const Phase = {
   Main: 'Main',
   End: 'End',
 } as const;
-type Phase = typeof Phase[keyof typeof Phase];
+type Phase = (typeof Phase)[keyof typeof Phase];
 
 function App() {
   const [phase, setPhase] = useState<Phase>(Phase.Main);
-  const setNextPhase = () => setPhase(phase => phase == Phase.End ? Phase.Main : Phase.End);
+  const setNextPhase = () =>
+    setPhase(p => (p === Phase.End ? Phase.Main : Phase.End));
   return (
     <>
       <h1>Wartide</h1>
-      <h4 id='phase'>Phase</h4>
-      <p aria-labelledby='phase'>{phase}</p>
+      <h4 id="phase">Phase</h4>
+      <p aria-labelledby="phase">{phase}</p>
       <button onClick={setNextPhase}>Next phase</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
