4df6d0a76b1185c4d9d2ab58683ae6084eb9c61c
Emily Pummeroy <emilypummeroy@gmail.com>
2026-03-27 13:21:59 +1100

    1: Add a phase indicator and Next Phase button
    - The initial phase is the main phase
    - It cycles between two phases: Main and End

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,5 @@
-import '@testing-library/jest-dom/vitest';
 import { describe, it, expect, beforeEach } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App.tsx';
 
 describe('the dom test environment', () => {
@@ -12,8 +11,35 @@ describe('the dom test environment', () => {
 describe(App, () => {
   beforeEach(() => render(<App />));
 
-  it("should have the heading", async () => {
-    expect(await screen.findByRole('heading')).toHaveTextContent('Wartide');
+  it("should have the wartide heading", async () => {
+    const heading = await screen.findByRole('heading', { level: 1 });
+
+    expect(heading).toHaveTextContent('Wartide');
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Wartide');
+  });
+
+  describe('Phase', () => {
+    it("should start in the main phase", async () => {
+      expect(await screen.findByLabelText('Phase')).toHaveTextContent('Main');
+    });
+
+    it("should advance to the end phase when the button is clicked", async () => {
+      fireEvent.click(await screen.findByText('Next phase'));
+
+      expect(screen.getByLabelText('Phase')).toHaveTextContent('End');
+    });
+
+    it("should cycle between main and end phase when the button is clicked", async () => {
+      for await (let _ of Array(3)) {
+        fireEvent.click(await screen.findByText('Next phase'));
+
+        expect(screen.getByLabelText('Phase')).toHaveTextContent('End');
+
+        fireEvent.click(await screen.findByText('Next phase'));
+
+        expect(screen.getByLabelText('Phase')).toHaveTextContent('Main');
+      }
+    });
   });
 });
   
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import './App.css'
 
+const Phase = {
+  Main: 'Main',
+  End: 'End',
+} as const;
+type Phase = typeof Phase[keyof typeof Phase];
+
 function App() {
+  const [phase, setPhase] = useState<Phase>(Phase.Main);
+  const setNextPhase = () => setPhase(phase => phase == Phase.End ? Phase.Main : Phase.End);
   return (
     <>
       <h1>Wartide</h1>
+      <h4 id='phase'>Phase</h4>
+      <p aria-labelledby='phase'>{phase}</p>
+      <button onClick={setNextPhase}>Next phase</button>
     </>
   )
 }
