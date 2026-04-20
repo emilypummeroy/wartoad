17037d29d606cf519b9f52ad090ab04c55557f46
Emily Pummeroy <emilypummeroy@gmail.com>
2026-04-20 11:52:42 +1000

    -: Add basic consumer test for GameContext

---
+++ src/context/GameContext.test.tsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+import { useContext } from 'react';
+
+import { CardClass } from '../types/card-class';
+import { GameContext, type GameDispatch } from './GameContext';
+
+describe(GameContext, () => {
+  describe('when consumed without a context provider', () => {
+    function TestConsumer() {
+      const [, dispatch] = useContext(GameContext);
+
+      for (const x of Object.values(dispatch)) expect(x).not.toThrow();
+
+      const { endPhase, pickCard, placeCard } = dispatch;
+      expect(() => endPhase()).not.toThrow();
+      expect(() => pickCard(CardClass.LilyPad)).not.toThrow();
+      expect(() => placeCard({ x: 0, y: 0 })).not.toThrow();
+      ({ endPhase, pickCard, placeCard }) satisfies GameDispatch;
+
+      return '';
+    }
+
+    it('should not throw anything when default dispatch functions are called', () => {
+      render(<TestConsumer />);
+    });
+  });
+});
