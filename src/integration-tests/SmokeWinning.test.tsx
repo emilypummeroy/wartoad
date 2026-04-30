88636ebc2cc0219d2ba3ed59518ec348dbd36ea9
Emily Pummeroy <emilypummeroy@gmail.com>
2026-04-30 14:30:46 +1000

    15: Add a smoke test for the simultaneous victory case

--- src/integration-tests/SmokeWinning.test.tsx
+++ src/integration-tests/SmokeWinning.test.tsx
@@ -35,4 +35,51 @@ describe('Smoke test: Advancing units and Capturing leaves', () => {
       expect(getThe.winrar(player)).toBeVisible();
     });
   });
+
+  describe.for([North, South])(
+    'after %s has deployed a units on their home and both players have deployed and advanced another unit',
+    player => {
+      const opponent = player === North ? South : North;
+
+      const moveUnitFromRowToRow = (owner: Player, start: number, end: number) => {
+        advanceToPhase(owner, Main);
+        fireEvent.click(getThe.nthRowUnitControlledBy(owner, start));
+        fireEvent.click(getThe.nthRowDropzoneFor(owner, end));
+      };
+
+      beforeEach(() => {
+        // Have opponent deploy and advance one unit.
+        advanceToPhase(opponent, Main);
+        fireEvent.click(getFirst.handCardNamed(opponent, Froglet.name));
+        fireEvent.click(getThe.homeLeafDropzone(opponent));
+        moveUnitFromRowToRow(opponent, 0, 1);
+
+        // Have player deploy and advance one unit.
+        advanceToPhase(player, Main);
+        fireEvent.click(getFirst.handCardNamed(player, Froglet.name));
+        fireEvent.click(getThe.homeLeafDropzone(player));
+        moveUnitFromRowToRow(player, 0, 1);
+        // Have player deploy an additional unit.
+        fireEvent.click(getFirst.handCardNamed(player, Froglet.name));
+        fireEvent.click(getThe.homeLeafDropzone(player));
+      });
+
+      it(`should allow ${opponent} to win if ${player} ends a turn with both Homes being capturable`, () => {
+        // Move a unit of each player into place at the other's Home
+        // but don't end the last player turn yet.
+        for (let i = 1; i < ROW_COUNT - 1; i += 1) {
+          moveUnitFromRowToRow(opponent, i, i + 1);
+          moveUnitFromRowToRow(player, i, i + 1);
+        }
+
+        // Have player vacate their own home leaf so both homes
+        // are capturable.
+        fireEvent.click(getThe.nthRowUnitControlledBy(player, 0));
+        fireEvent.click(getFirst.basicDropzoneControlledBy(player));
+
+        advanceToGameOver(opponent);
+        expect(getThe.winrar(opponent)).toBeVisible();
+      });
+    },
+  );
 });
