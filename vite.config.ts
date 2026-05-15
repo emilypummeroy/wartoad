8d26b14cdfc0b2d8380247059c5d358ef6810a03
Emily Pummeroy <emilypummeroy@gmail.com>
2026-05-15 10:01:18 +1000

    ~: Add @ path alias for src directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 /// <reference types="vitest/config" />
 
+import path from 'path';
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react()],
   test: {
     environment: 'jsdom',
